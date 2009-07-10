67df332a1bbb40bda41f6cf79cb7bd73c97c9931
Clint Hill <clint.hill@h3osoftware.com>
2009-07-10 12:10:45 -0700

    Updated version to 0.0.6

--- lib/siffer.rb
+++ lib/siffer.rb
@@ -8,7 +8,7 @@ $: << File.expand_path(File.dirname(__FILE__))
 module Siffer
 
     VENDOR = "h3o(software)" unless defined?(Siffer::VENDOR)
-    VERSION = [0,0,5] unless defined?(Siffer::VERSION)
+    VERSION = [0,0,6] unless defined?(Siffer::VERSION)
     SIF_VERSION = [2,3,0] unless defined?(Siffer::SIF_VERSION)
     SIF_XMLNS = "http://www.sifinfo.org/infrastructure/2.x" unless defined?(Siffer::SIF_XMLNS)
   
